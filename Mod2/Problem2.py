0e447878810157b1a6110a4f057d2f6703734dd9
austin fitzhugh <156254191+austinfitzhugh@users.noreply.github.com>
2025-04-19 22:28:35 -0700

    Create Problem2.py

---
+++ Mod2/Problem2.py
@@ -0,0 +1,8 @@
+# Create a program that prompts for 2 numbers and then outputs the addition, subtraction, multiplication, and division of the first number by the second number.
+
+num1 = int(input("please enter a number "))
+num2 = int(input("please enter another number "))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 / num2)
