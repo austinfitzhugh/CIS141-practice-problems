65f73ec229cf609ebc115ac56bf1b20b2ad00285
austin fitzhugh <156254191+austinfitzhugh@users.noreply.github.com>
2025-05-18 18:25:11 -0700

    Create problem2.py

---
+++ Mod6/problem2.py
@@ -0,0 +1,4 @@
+#2. Create a list of strings. Write code that counts how many times the word "Olympic" appears in the list, and then print the count.
+ls = ["Olympic","College","Bremerton"]
+Olympic_amount = ls.count("Olympic")
+print(f"This string say Olympic", Olympic_amount, "times")
