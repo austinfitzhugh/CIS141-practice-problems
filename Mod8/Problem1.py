07e39e9106170b957211c54bb748a8d1b71211a1
austin fitzhugh <156254191+austinfitzhugh@users.noreply.github.com>
2025-06-01 21:11:11 -0700

    Create Problem1.py

---
+++ Mod8/Problem1.py
@@ -0,0 +1,9 @@
+#1. Create a file called gardening_tips.txt and add at least 3 gardening tips to
+#it. Write a Python script that reads a file gardening_tips.txt and prints
+#out each tip one by one.
+
+with open("gardening_tips.txt", "r") as file:
+    tips = file.readlines()
+    
+for tip in tips:
+    print(tip.strip())
